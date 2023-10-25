ba8963cc86a6094b60d660936c776d847811ae37
Andrewsjjz <andresjimenez884@gmail.com>
2023-10-25 18:20:11 -0400

    continuacion de los links

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,10 +1,261 @@
-
 const Orders = () => {
   return (
-    <div>
-      Desde ordenes
-    </div>
-  )
-}
+    <>
+      <div className="p-5 lg:grid lg:grid-cols-3 gap-5">
 
-export default Orders
+        <div className="max-w-sm w-full lg:max-w-full lg:flex mb-4">
+          <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
+            <img
+              src="https://media-cdn.tripadvisor.com/media/photo-s/19/1e/1a/3a/pizza-hut.jpg"
+              alt=""
+            />
+          </div>
+          <div
+            className="border-r border-b border-l border-gray-400 lg:border-l-0 
+  lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r 
+  flex flex-col justify-between leading-normal"
+          >
+            <div className="flex justify-between items-center">
+              <p className="text-xl mr-10">Pizza peperoni</p>
+              <svg
+                viewBox="0 0 1024 1024"
+                fill="green"
+                height="2em"
+                width="2em"
+              >
+                <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM695.5 365.7l-210.6 292a31.8 31.8 0 01-51.7 0L308.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H689c6.5 0 10.3 7.4 6.5 12.7z" />
+              </svg>
+            </div>
+            <p className="text-gray-900 font-bold text-xl mb-2">
+              Pizza la nacional
+            </p>
+            <p className="text-gray-700">04/02/2022 - 19:34</p>
+            <p className="text-gray-900 ">$ 18,55 / 3</p>
+
+            <div className="flex justify-between items-center">
+              <p className="text-gray-900 ">View details</p>
+              <svg
+                viewBox="0 0 64 64"
+                fill="black"
+                height="1em"
+                width="1em"
+              >
+                <path
+                  fill="none"
+                  stroke="black"
+                  strokeLinejoin="bevel"
+                  strokeMiterlimit={10}
+                  strokeWidth={2}
+                  d="M27 15l17 17-17 17"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+
+        <div className="max-w-sm w-full lg:max-w-full lg:flex mb-4">
+          <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
+            <img
+              src="https://media-cdn.tripadvisor.com/media/photo-s/19/1e/1a/3a/pizza-hut.jpg"
+              alt=""
+            />
+          </div>
+          <div
+            className="border-r border-b border-l border-gray-400 lg:border-l-0 
+  lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r 
+  flex flex-col justify-between leading-normal"
+          >
+            <div className="flex justify-between items-center">
+              <p className="text-xl mr-10">Pizza peperoni</p>
+              <svg
+                viewBox="0 0 1024 1024"
+                fill="green"
+                height="2em"
+                width="2em"
+              >
+                <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM695.5 365.7l-210.6 292a31.8 31.8 0 01-51.7 0L308.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H689c6.5 0 10.3 7.4 6.5 12.7z" />
+              </svg>
+            </div>
+            <p className="text-gray-900 font-bold text-xl mb-2">
+              Pizza la nacional
+            </p>
+            <p className="text-gray-700">04/02/2022 - 19:34</p>
+            <p className="text-gray-900 ">$ 18,55 / 3</p>
+
+            <div className="flex justify-between items-center">
+              <p className="text-gray-900 ">View details</p>
+              <svg
+                viewBox="0 0 64 64"
+                fill="black"
+                height="1em"
+                width="1em"
+              >
+                <path
+                  fill="none"
+                  stroke="black"
+                  strokeLinejoin="bevel"
+                  strokeMiterlimit={10}
+                  strokeWidth={2}
+                  d="M27 15l17 17-17 17"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+
+        <div className="max-w-sm w-full lg:max-w-full lg:flex mb-4">
+          <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
+            <img
+              src="https://media-cdn.tripadvisor.com/media/photo-s/19/1e/1a/3a/pizza-hut.jpg"
+              alt=""
+            />
+          </div>
+          <div
+            className="border-r border-b border-l border-gray-400 lg:border-l-0 
+  lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r 
+  flex flex-col justify-between leading-normal"
+          >
+            <div className="flex justify-between items-center">
+              <p className="text-xl mr-10">Pizza peperoni</p>
+              <svg
+                viewBox="0 0 1024 1024"
+                fill="green"
+                height="2em"
+                width="2em"
+              >
+                <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM695.5 365.7l-210.6 292a31.8 31.8 0 01-51.7 0L308.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H689c6.5 0 10.3 7.4 6.5 12.7z" />
+              </svg>
+            </div>
+            <p className="text-gray-900 font-bold text-xl mb-2">
+              Pizza la nacional
+            </p>
+            <p className="text-gray-700">04/02/2022 - 19:34</p>
+            <p className="text-gray-900 ">$ 18,55 / 3</p>
+
+            <div className="flex justify-between items-center">
+              <p className="text-gray-900 ">View details</p>
+              <svg
+                viewBox="0 0 64 64"
+                fill="black"
+                height="1em"
+                width="1em"
+              >
+                <path
+                  fill="none"
+                  stroke="black"
+                  strokeLinejoin="bevel"
+                  strokeMiterlimit={10}
+                  strokeWidth={2}
+                  d="M27 15l17 17-17 17"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+
+        <div className="max-w-sm w-full lg:max-w-full lg:flex mb-4">
+          <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
+            <img
+              src="https://media-cdn.tripadvisor.com/media/photo-s/19/1e/1a/3a/pizza-hut.jpg"
+              alt=""
+            />
+          </div>
+          <div
+            className="border-r border-b border-l border-gray-400 lg:border-l-0 
+  lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r 
+  flex flex-col justify-between leading-normal"
+          >
+            <div className="flex justify-between items-center">
+              <p className="text-xl mr-10">Pizza peperoni</p>
+              <svg
+                viewBox="0 0 1024 1024"
+                fill="green"
+                height="2em"
+                width="2em"
+              >
+                <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM695.5 365.7l-210.6 292a31.8 31.8 0 01-51.7 0L308.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H689c6.5 0 10.3 7.4 6.5 12.7z" />
+              </svg>
+            </div>
+            <p className="text-gray-900 font-bold text-xl mb-2">
+              Pizza la nacional
+            </p>
+            <p className="text-gray-700">04/02/2022 - 19:34</p>
+            <p className="text-gray-900 ">$ 18,55 / 3</p>
+
+            <div className="flex justify-between items-center">
+              <p className="text-gray-900 ">View details</p>
+              <svg
+                viewBox="0 0 64 64"
+                fill="black"
+                height="1em"
+                width="1em"
+              >
+                <path
+                  fill="none"
+                  stroke="black"
+                  strokeLinejoin="bevel"
+                  strokeMiterlimit={10}
+                  strokeWidth={2}
+                  d="M27 15l17 17-17 17"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+
+        <div className="max-w-sm w-full lg:max-w-full lg:flex mb-4">
+          <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
+            <img
+              src="https://media-cdn.tripadvisor.com/media/photo-s/19/1e/1a/3a/pizza-hut.jpg"
+              alt=""
+            />
+          </div>
+          <div
+            className="border-r border-b border-l border-gray-400 lg:border-l-0 
+  lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r 
+  flex flex-col justify-between leading-normal"
+          >
+            <div className="flex justify-between items-center">
+              <p className="text-xl mr-10">Pizza peperoni</p>
+              <svg
+                viewBox="0 0 1024 1024"
+                fill="green"
+                height="2em"
+                width="2em"
+              >
+                <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM695.5 365.7l-210.6 292a31.8 31.8 0 01-51.7 0L308.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H689c6.5 0 10.3 7.4 6.5 12.7z" />
+              </svg>
+            </div>
+            <p className="text-gray-900 font-bold text-xl mb-2">
+              Pizza la nacional
+            </p>
+            <p className="text-gray-700">04/02/2022 - 19:34</p>
+            <p className="text-gray-900 ">$ 18,55 / 3</p>
+
+            <div className="flex justify-between items-center">
+              <p className="text-gray-900 ">View details</p>
+              <svg
+                viewBox="0 0 64 64"
+                fill="black"
+                height="1em"
+                width="1em"
+              >
+                <path
+                  fill="none"
+                  stroke="black"
+                  strokeLinejoin="bevel"
+                  strokeMiterlimit={10}
+                  strokeWidth={2}
+                  d="M27 15l17 17-17 17"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </>
+  );
+};
+
+export default Orders;
